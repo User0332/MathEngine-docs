2121f0db39b7054c3ec971585ea5cfdb229d3d53
Carl Furtado <carlzfurtado@gmail.com>
2025-01-01 17:30:08 -0500

    add conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'MathEngine'
+copyright = '2025, Carl Furtado'
+author = 'Carl Furtado'
+
+release = 'latest'
+
+# -- General configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme'
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
